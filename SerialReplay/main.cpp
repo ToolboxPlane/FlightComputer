625680deccf561abadc30e4afdff21aac6f28e18
Paul Nykiel <paul.nykiel@gmail.com>
2018-01-07 20:35:44 +0100

    Cleanup, CLI

--- SerialReplay/main.cpp
+++ SerialReplay/main.cpp
@@ -3,6 +3,7 @@
 #include <QTextStream>
 #include <QDateTime>
 #include <QtDBus>
+#include <QCommandLineParser>
 #include <QDebug>
 
 struct RecordingLine {
@@ -25,8 +26,10 @@ RecordingLine parseLine(QString line) {
     return ret;
 }
 
-void sendData(RecordingLine data, QString name, QString objectpath) {
-    qDebug() << "Sending: " << data.timestamp << data.sender << data.signalName << data.data;
+void sendData(RecordingLine data, QString name, QString objectpath, bool debug) {
+    if(debug) {
+        qDebug() << "Sending: " << data.timestamp << data.sender << data.signalName << data.data;
+    }
     QStringList signalSep = data.signalName.split(".");
     QString realSignalName = signalSep[signalSep.size()-1];
     QDBusMessage msg = QDBusMessage::createSignal(objectpath,name, realSignalName);
@@ -38,8 +41,22 @@ void sendData(RecordingLine data, QString name, QString objectpath) {
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QCoreApplication::setApplicationName("SerialReplay");
+    QCoreApplication::setApplicationVersion("1.0");
 
-    QFile inputFile("../SerialReplay/testrecording");
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Replays a recording");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("input", "One or more recording files");
+    parser.addOptions({
+        {{"n", "norealtime"}, "Replay all signals imediatly"},
+        {{"d", "debug"}, "Print more debug information"}
+    });
+
+    parser.process(a);
+
+    QFile inputFile(parser.positionalArguments()[0]);
     if (!inputFile.open(QIODevice::ReadOnly))
     {
         qDebug() << "Error opening File!";
@@ -57,7 +74,7 @@ int main(int argc, char *argv[])
     qint64 startTime = QDateTime::currentDateTime().toMSecsSinceEpoch()/1000;
     qint64 recordingStartTime = begin.timestamp;
 
-    sendData(begin, name, objectpath);
+    sendData(begin, name, objectpath, parser.isSet("d"));
 
     while (!in.atEnd())
     {
@@ -65,13 +82,13 @@ int main(int argc, char *argv[])
 
         qint64 recordingDiff = currLine.timestamp - recordingStartTime;
         qint64 realDiff = QDateTime::currentDateTime().toMSecsSinceEpoch()/1000 - startTime;
-        while (realDiff < recordingDiff){
+        while (!parser.isSet("n") && realDiff < recordingDiff){
             realDiff = QDateTime::currentDateTime().toMSecsSinceEpoch()/1000 - startTime;
         }
-        sendData(currLine, name, objectpath);
+        sendData(currLine, name, objectpath, parser.isSet("d"));
     }
     inputFile.close();
     qDebug() << "Replay finished!";
-    return a.exec();
+    return 0;
 }
 
