9075b7499ed031767ce9101b85b4142ef908e346
Paul Nykiel <paul.nykiel@gmail.com>
2018-01-19 02:04:32 +0100

    Added json encoding

--- LoRaDbus/main.cpp
+++ LoRaDbus/main.cpp
@@ -1,6 +1,8 @@
 #include <QCoreApplication>
 #include <QtDBus>
 #include <QDebug>
+#include <QJsonObject>
+#include <QJsonDocument>
 #include "LoRaHal.h"
 #include "RadioControlProtocol/rcLib.hpp"
 
@@ -18,9 +20,28 @@ int main(int argc, char *argv[])
         for(size_t c=0; c<lora.getLength(); c++) {
             if(pkgIn.decode(lora.getData()[c])) {
                 qDebug() << "New Package " << pkgIn.getChannel(0) << pkgIn.getChannel(1) << pkgIn.getChannel(2);
+                QJsonObject json;
+                QJsonObject header, config, channel;
+                header["Uid"] = 0;
+                header["TransmitterId"] = pkgIn.getDeviceId();
+
+                config["ChannelCount"] = pkgIn.getChannelCount();
+                config["Resolution"] = pkgIn.getResolution();
+                config["Error"] = false;
+                config["IsMeshPackage"] = pkgIn.isMesh();
+                config["Mesh"] = 0;
+                config["RoutingLength"] = 0;
+
+                json["Header"] = header;
+                json["Config"] = config;
+                json["Channel"] = channel;
+
+                QJsonDocument doc(json);
+                QString payload(doc.toJson(QJsonDocument::Compact));
+
                 QDBusMessage msg = QDBusMessage::createSignal(
                     "/loraData", "de.toolboxbodensee.plane", "onRcPackageReceived");
-                QVariant content(pkgIn.getChannel(0)+" "+pkgIn.getChannel(1));
+                QVariant content(payload);
                 msg << content;
                 QDBusConnection::systemBus().send(msg);
             }
