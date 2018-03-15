8e13ec6d9f656a3340372b964a942c3483369861
Paul Nykiel <paul.nykiel@gmail.com>
2018-03-15 15:04:31 +0100

    Structure for new modules

---
+++ Devices/SerialSimulator.hpp
@@ -0,0 +1,26 @@
+//
+// Created by paul on 14.03.18.
+//
+
+#ifndef FLIGHTCOMPUTER_SERIALSIMULATOR_HPP
+#define FLIGHTCOMPUTER_SERIALSIMULATOR_HPP
+
+
+#include "../Channel.hpp"
+#include "RadioControlProtocol/rcLib.hpp"
+#include "../ThreadModule.hpp"
+
+class SerialSimulator : public ThreadModule{
+public:
+    SerialSimulator() : ThreadModule() {};
+    void run() override;
+
+    Channel<rcLib::Package> &getChannelIn();
+    Channel<rcLib::Package> &getChannelOut();
+
+private:
+    Channel<rcLib::Package> in, out;
+};
+
+
+#endif //FLIGHTCOMPUTER_SERIALSIMULATOR_HPP
