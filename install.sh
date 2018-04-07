b2f5485cca9d58fd1c7a8ad7e522ed29b4bd3df0
Flightcomputer <flight@computer.bullshit>
2018-04-07 15:44:41 +0000

    Updated the installation script

--- install.sh
+++ install.sh
@@ -9,18 +9,24 @@ git clone https://github.com/hallard/RadioHead /home/pi/RadioHead
 # Install bcm2835 Library (Library for the CPU of the Pi)
 # See also: http://www.airspayce.com/mikem/bcm2835/
 wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.55.tar.gz
-tar zxvf bcm2835-1.52.tar.gz
-cd bcm2835-1.52
+tar zxvf bcm2835-1.55.tar.gz
+cd bcm2835-1.55
 ./configure
 make -j4
 sudo make check
 sudo make install
 
+# Generate a ssh-key
+ssh-keygen
+cat .ssh/id-rsa.pub
+
 # Clone this repo
 git clone git@github.com:ToolboxPlane/FlightComputer.git
+cd FlightComputer
+git submodule init
+git submodule update
 
 # Compile the program
-cd FlightComputer
 mkdir build
 cd build
 cmake ..
