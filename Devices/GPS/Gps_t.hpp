585ac7ba18eca02e31a4182d4d241292172cdd41
Paul Nykiel <paul.nykiel@gmail.com>
2018-05-14 17:03:50 +0200

    Gps_t should be working

--- Devices/GPS/Gps_t.hpp
+++ Devices/GPS/Gps_t.hpp
@@ -15,28 +15,36 @@ public:
     double altitude;
 
     double distanceTo(Gps_t gps) {
-        double phi1 = this->lat / 180.0 * M_PI;
-        double phi2 = gps.lat / 180.0 * M_PI;
-        double delta_phi = phi2 - phi1;
-        double delta_lambda = (gps.lon-this->lon) / 180.0 * M_PI;
+        double phi1 = this->lon / 180 * M_PI;
+        double sigma1 = M_PI/2 - (this->lat / 180 * M_PI);
+        double r1 = EARTH_RADIUS + this->altitude;
+        double x1 = r1 * std::sin(sigma1) * std::cos(phi1);
+        double y1 = r1 * std::sin(sigma1) * std::sin(phi1);
+        double z1 = r1 * std::cos(sigma1);
 
-        double a = std::sin(delta_phi/2) * std::sin(delta_phi/2) +
-                   std::cos(phi1) * std::cos(phi2) *
-                   std::sin(delta_lambda/2) * std::sin(delta_lambda/2);
-        double c = 2 * std::atan2(std::sqrt(a), std::sqrt(1-a));
+        double phi2 = gps.lon / 180 * M_PI;
+        double sigma2 = M_PI/2 - (gps.lat / 180 * M_PI);
+        double r2 = EARTH_RADIUS + gps.altitude;
+        double x2 = r2 * std::sin(sigma2) * std::cos(phi2);
+        double y2 = r2 * std::sin(sigma2) * std::sin(phi2);
+        double z2 = r2 * std::cos(sigma2);
 
-        return EARTH_RADIUS * c;
+        double dx = x1 - x2;
+        double dy = y1 - y2;
+        double dz = z1 - z2;
+
+        return std::sqrt(dx*dx + dy*dy + dz*dz);
     }
 
     double angleTo(Gps_t gps) {
         Gps_t supportPoint(this->lat, gps.lon);
         double xDiff = this->distanceTo(supportPoint);
         double yDiff = supportPoint.distanceTo(gps);
-        return std::atan2(yDiff, xDiff) * 180.0 / M_PI;
+        return -std::atan2(yDiff, xDiff) * 180.0 / M_PI + 90;
     }
 
 private:
-    static constexpr double EARTH_RADIUS = 63713;
+    static constexpr double EARTH_RADIUS = 6371300;
 };
 
 
