8aecbaa53fc6f7b682a401ff0b273efec23eae33
Flightcomputer <flight@computer.bullshit>
2018-07-29 20:14:28 +0000

    Updated the installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,19 +3,11 @@
 # Install necessary software
 sudo apt install -y build-essential git cmake libgps-dev gpsd
 
-# Install Radiohead
-git clone https://github.com/hallard/RadioHead /home/pi/RadioHead
-
-# Install bcm2835 Library (Library for the CPU of the Pi)
-# See also: http://www.airspayce.com/mikem/bcm2835/
-wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.55.tar.gz
-tar zxvf bcm2835-1.55.tar.gz
-cd bcm2835-1.55
-./configure
-make -j4
-sudo make check
-sudo make install
-cd ~
+# Install wiring pi
+git clone git://git.drogon.net/wiringPi
+cd wiringPi
+./build
+cd ..
 
 # Generate a ssh-key
 ssh-keygen
